83be162d27dc4aee056c8b5b9674714a0081d98b
Silesh K Nair <sileshn@gmail.com>
2022-05-26 11:23:17 +0530

    Fix merge

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -4744,7 +4744,7 @@ int mmc_flush_cache(struct mmc_card *card)
 		err = mmc_switch(card, EXT_CSD_CMD_SET_NORMAL,
 				EXT_CSD_FLUSH_CACHE, 1,
 				 MMC_CACHE_FLUSH_TIMEOUT_MS);
-		if (err)
+		if (err)  {
 			pr_err("%s: cache flush error %d\n",
 					mmc_hostname(card->host), err);
 		}
